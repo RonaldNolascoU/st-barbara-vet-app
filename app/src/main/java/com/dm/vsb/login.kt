a3b2db0a2da26e8656af2cb6b16be0cd99752b93
edwinmejia-123 <edwin_adonay123@hotmail.com>
2024-05-10 13:18:22 -0600

    Diseño de login y registro

---
+++ app/src/main/java/com/dm/vsb/login.kt
@@ -0,0 +1,42 @@
+package com.dm.vsb
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val usernameEditText: EditText = findViewById(R.id.editTextUsername)
+        val passwordEditText: EditText = findViewById(R.id.editTextPassword)
+        val loginButton: Button = findViewById(R.id.buttonLogin)
+        val textViewRegister: TextView = findViewById(R.id.textViewRegister)
+        textViewRegister.setOnClickListener {
+            val intent = Intent(this, register::class.java)
+            startActivity(intent)
+        }
+        loginButton.setOnClickListener {
+            val username = usernameEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            if (username == "user" && password == "pass") {
+                // Código para manejar el inicio de sesión exitoso
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()  // Cierra la actividad actual
+            } else {
+                // Manejar inicio de sesión fallido
+                Toast.makeText(this, "Credenciales incorrectas", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
