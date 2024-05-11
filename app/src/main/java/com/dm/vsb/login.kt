6d6f06426cd2c5a16580c64eec023540fd1a09a7
Ronald Nolasco <30516460+RonaldNolascoU@users.noreply.github.com>
2024-05-11 16:59:37 -0600

    Firebase login (#1)

--- app/src/main/java/com/dm/vsb/login.kt
+++ app/src/main/java/com/dm/vsb/login.kt
@@ -2,6 +2,7 @@ package com.dm.vsb
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -11,10 +12,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.auth
+import com.google.firebase.Firebase
 
 class login : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        auth = Firebase.auth
 
         Thread.sleep(3000)
         installSplashScreen()
@@ -32,15 +40,76 @@ class login : AppCompatActivity() {
             val username = usernameEditText.text.toString()
             val password = passwordEditText.text.toString()
 
-            if (username == "user" && password == "pass") {
-                // Código para manejar el inicio de sesión exitoso
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-                finish()  // Cierra la actividad actual
+            if (username.isEmpty() || password.isEmpty()) {
+                Toast.makeText(
+                    baseContext,
+                    "Ingrese usuario y/o contraseña",
+                    Toast.LENGTH_SHORT
+                ).show()
             } else {
-                // Manejar inicio de sesión fallido
-                Toast.makeText(this, "Credenciales incorrectas", Toast.LENGTH_SHORT).show()
+                signIn(username, password)
             }
         }
     }
+
+    // [START on_start_check_user]
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            // TODO: Descomentar esto, de momento está asi porque no hay logout
+            // reload()
+        }
+    }
+    // [END on_start_check_user]
+
+    private fun signIn(email: String, password: String) {
+        // [START sign_in_with_email]
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    val intent = Intent(this, MainActivity::class.java)
+                     startActivity(intent)
+                     finish()  // Cierra la actividad actual
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                    updateUI(null)
+                }
+            }
+        // [END sign_in_with_email]
+    }
+
+    private fun sendEmailVerification() {
+        // [START send_email_verification]
+        val user = auth.currentUser!!
+        user.sendEmailVerification()
+            .addOnCompleteListener(this) { task ->
+                // Email Verification sent
+            }
+        // [END send_email_verification]
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+    }
+
+    private fun reload() {
+         val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()  // Cierra la actividad actual
+    }
+
+    companion object {
+        private const val TAG = "EmailPasswordLogin"
+    }
+
 }
